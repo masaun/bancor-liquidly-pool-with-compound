3afff41b2c7e31e156b3818022394f8696aa61cc
masaun <masanoriuno@gmail.com>
2020-02-04 20:37:54 +0100

    Add migration files for deploying bancor-protocol contract

---
+++ migrations/3_deploy_bancor-protocol_1.js
@@ -0,0 +1,9 @@
+var SmartToken = artifacts.require("SmartToken");
+const _name = 'Test SmartToken'
+const _symbol = 'TST'
+const _decimals 18
+
+
+module.exports = async function(deployer) {
+  await deployer.deploy(SmartToken, _name, _symbol, _decimals);
+};
---
+++ migrations/4_deploy_bancor-protocol_2.js
@@ -0,0 +1,15 @@
+var BancorConverter = artifacts.require("BancorConverter");
+var SmartToken = artifacts.require("SmartToken");
+const _token = SmartToken.address
+const _registry = '0x8a69A7d7507F8c4a9dD5dEB9B687B30D2b30A011'
+const _maxConversionFee = 1
+const _reserveToken = '0xad6d458402f60fd3bd25163575031acdce07538d'  // DAI on Ropsten
+const _reserveRatio = 10
+
+var BancorConverterFactory = artifacts.require("BancorConverterFactory");
+
+
+module.exports = async function(deployer) {
+  await deployer.deploy(BancorConverter, _token, _registry, _maxConversionFee, _reserveToken, _reserveRatio);
+  await deployer.deploy(BancorConverterFactory);
+};
