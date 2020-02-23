4d2c2c38c8255129b9d25576bbc1a4a51832a0b1
Andre Cronje <andre@fantom.foundation>
2020-02-23 07:09:07 +0200

    iearn.finance integration for Bancor liquidity pools
    Signed-off-by: Andre Cronje <andre@fantom.foundation>

--- contracts/NewBancorPool.sol
+++ contracts/NewBancorPool.sol
@@ -21,8 +21,8 @@ import './bancor-protocol/utility/Managed.sol';
 import './bancor-protocol/converter/BancorFormula.sol';
 
 
-// Compound and CToken
-import "./compound-protocol/interfaces/ICErc20.sol";
+// iearn.finance and YToken
+import "./iearn-protocol/interfaces/IYErc20.sol";
 //import "./compound-protocol/CErc20.sol";
 
 
@@ -44,7 +44,7 @@ contract NewBancorPool is BnStorage, BnConstants, Managed {
     BancorFormula public bancorFormula;
 
     IERC20Token public iErc20;  // i.e). DAI
-    ICErc20 public iCErc20;     // i.e). cDAI（cToken）from compound pool
+    IYErc20 public iYErc20;     // i.e). yDAI（yToken）from iearn pool
 
     address contractRegistryAddr;
 
@@ -66,7 +66,7 @@ contract NewBancorPool is BnStorage, BnConstants, Managed {
         //address _bancorConverterFactory,
         address _bancorConverterRegistry,
         address _bancorFormula
-    ) 
+    )
         public
     {
         // Step #1: Initial Setup
@@ -83,7 +83,7 @@ contract NewBancorPool is BnStorage, BnConstants, Managed {
         bancorConverterAddr = _bancorConverter;
 
         iErc20 = IERC20Token(ERC20tokenAddr);   // DAI on Ropsten
-        iCErc20 = ICErc20(cDAItokenAddr);       // cDAI on Ropsten
+        iYErc20 = IYErc20(cDAItokenAddr);       // cDAI on Ropsten
         //iCErc20 = ICErc20(0x2B536482a01E620eE111747F8334B395a42A555E);  // cDAI on Ropsten
 
         // Step #2: Smart Relay Token Deployment
@@ -112,28 +112,28 @@ contract NewBancorPool is BnStorage, BnConstants, Managed {
 
 
     /***
-     * @notice - Mint cToken (Compound Token)
-     * @dev - Reference from this link => https://compound.finance/developers/ctokens
+     * @notice - Mint yToken (iearn Token)
+     * @dev - Reference from this link => https://docs.iearn.finance/yield-tokens/ydai
      ***/
-    function mintCToken(uint256 mintAmount, address recipient) public returns (bool) {
+    function mintYToken(uint256 mintAmount, address recipient) public returns (bool) {
         iErc20.approve(address(cDAItokenAddr), mintAmount);   // approve the transfer
-        iCErc20.mint(mintAmount);
+        iYErc20.deposit(mintAmount);
 
-        iCErc20.transfer(recipient, mintAmount);
+        iYErc20.transfer(recipient, iYErc20.balanceOf(address(this)));
 
-        // [Ref]: 
+        // [Ref]:
         // Erc20 underlying = Erc20(_ERC20tokenAddr);  // get a handle for the underlying asset contract
         // CErc20 cToken = CErc20(cDAItokenAddr);      // get a handle for the corresponding cToken contract
         // underlying.approve(address(cToken), 100);   // approve the transfer
         // assert(cToken.mint(100) == 0);              // mint the cTokens and assert there is no error
         return BnConstants.CONFIRMED;
     }
-    
+
 
 
 
     /***
-     * @notice - Integrate bancor pools with lending protocols (Compound) to hedge risk for stakers 
+     * @notice - Integrate bancor pools with lending protocols (Compound) to hedge risk for stakers
      * https://docs.bancor.network/user-guides/token-integration/how-to-create-a-bancor-liquidity-pool
      **/
     function bancorPoolWithCompound(
@@ -141,11 +141,11 @@ contract NewBancorPool is BnStorage, BnConstants, Managed {
         uint256 amountOfSmartToken
     ) public returns (bool) {
         // [In progress]: Integrate with lending pool of compound (cToken)
-        
+
         // Step #1: Initial Setup
         bytes32 BNT_TOKEN = "BNT Token";
         bytes32 CTOKEN = "compound DAI";
-        contractRegistry.registerAddress(BNT_TOKEN, BNTtokenAddr);  // BNT Token 
+        contractRegistry.registerAddress(BNT_TOKEN, BNTtokenAddr);  // BNT Token
         contractRegistry.registerAddress(CTOKEN, cDAItokenAddr);    // cToken (cDAI)
 
         address registryedToken1;
@@ -172,8 +172,8 @@ contract NewBancorPool is BnStorage, BnConstants, Managed {
         // Step #6: Multisig Ownership
         address _converterAddress;  // @notice - This variable is for receiving return value of createConverter() below
         // uint32 _maxConversionFee = 1;
-        bancorConverter.transferOwnership(msg.sender);   // @dev - Reference from Managed.sol 
-        bancorConverter.transferManagement(msg.sender);  // @dev - Reference from Managed.sol 
+        bancorConverter.transferOwnership(msg.sender);   // @dev - Reference from Managed.sol
+        bancorConverter.transferManagement(msg.sender);  // @dev - Reference from Managed.sol
         _converterAddress = address(bancorConverter);
 
         // Step #7: Converters Registry Listing
